20026e41d28d8f615135acd6dd78686f607c9bbc
julienverre85-glitch <julienverre85@gmail.com>
2026-04-30 07:28:10 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // ⚠️ Remplace YOUR_REPO_NAME par le nom exact de ton dépôt GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/YOUR_REPO_NAME/',
+  base: 'memonotes',
 })
